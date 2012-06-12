523f29efa6c213597c60efb33f043365fd23c860
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-06-12 17:46:34 +0000

    Slightly less hideous waste of whitespace
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheEntitiesPlugin/trunk@4772 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEntityAdmin/templates/_extraPageSettings.php
+++ modules/aEntityAdmin/templates/_extraPageSettings.php
@@ -1,8 +1,8 @@
 <?php // Include me from your a/extraPageSettings partial if you want to allow ?>
 <?php // entities to be associated with any page ?>
+<hr/>
 <?php foreach(aEntityTools::getClassInfos() as $class => $info): ?>
-  <hr/>
-  <h3><?php echo $info['plural'] ?></h3>
+  <h4><?php echo $info['plural'] ?></h4>
 
   <div class="a-options-section <?php echo $info['css'] ?> clearfix">
     <div class="a-form-row">
