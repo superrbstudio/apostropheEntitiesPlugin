7c023ccd7855acc7850acbcc88306c8e728bd2bf
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-10-19 18:44:55 +0000

    Use the new a_filter_dropdown helper
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheEntitiesPlugin/trunk@4867 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEntity/templates/classSuccess.php
+++ modules/aEntity/templates/classSuccess.php
@@ -15,43 +15,16 @@
 <div class="a-entity-controls">
   <h3>Sorting and Filtering Options</h3>
   <div>
-    <select id="alpha" name="alpha">
-      <option <?php echo ($sf_params->get('alpha') === 'asc') ? 'selected' : '' ?> value="asc">Sort ASCENDING (A-Z)</option>
-      <option <?php echo ($sf_params->get('alpha') === 'desc') ? 'selected' : '' ?> value="desc">Sort DESCENDING (Z-A)</option>
-    </select>
+    <?php echo a_filter_dropdown('alpha', array('choices' => array(
+    'asc' => 'Sort ASCENDING (A-Z)', 'desc' => 'Sort DESCENDING (Z-A)'))) ?>
   </div>
   <?php foreach ($filters as $filterClass => $filterInfo): ?>
     <?php $filterIds[] = $filterInfo['css'] ?>
     <?php $all = is_null($sf_params->get($filterInfo['css'], null)) ?>
 
     <div>
-      <select id="<?php echo $filterInfo['css'] ?>" name="<?php echo $filterInfo['css'] ?>">
-        <option <?php echo $all ? 'selected' : '' ?> value=""><?php echo $all ? 'Filter by ' . strtoupper($filterInfo['singular']) : ('All ' . strtoupper($filterInfo['plural'])) ?></option>
-        <?php foreach ($filterInfo['itemInfos'] as $itemInfo): ?>
-          <option <?php echo ($sf_params->get($filterInfo['css']) === $itemInfo['slug']) ? 'selected' : '' ?> value="<?php echo $itemInfo['slug'] ?>"><?php echo $itemInfo['name'] ?></option>
-        <?php endforeach ?>
-      </select>
+      <?php echo a_filter_dropdown($filterInfo['css'], array('choices' => $filterInfo['itemInfos'], 'valueColumn' => 'slug', 'labelColumn' => 'name', 'chooseOne' => 'Filter by ' . strtoupper($filterInfo['singular']), 'revertToAll' => 'All ' . strtoupper($filterInfo['plural']))) ?>
     </div>
   <?php endforeach ?>
 </div>
 
-<script type="text/javascript">
-$(function() {
-  var ids = <?php echo json_encode($filterIds) ?>;
-
-  $('#alpha').change(function() {
-    document.location.href = apostrophe.addParameterToUrl(document.location.href, 'alpha', $(this).val());
-  });
-
-  $.each(ids, function(index, id) {
-    $('#' + id).change(function() {
-      // Drop any page number when switching filters
-      document.location.href = apostrophe.addParameterToUrl(
-        apostrophe.addParameterToUrl(
-          document.location.href, id, $(this).val()),
-        'page', '');
-    });
-  });
-
-});
-</script>
