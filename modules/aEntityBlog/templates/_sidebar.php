b5dae958be23f63b2d5dd1c1e866bb95b5044837
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-12-14 21:18:32 +0000

    When a filter is already selected, toggle open only the appropriate subclass filter
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheEntitiesPlugin/trunk@4969 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEntityBlog/templates/_sidebar.php
+++ modules/aEntityBlog/templates/_sidebar.php
@@ -6,7 +6,13 @@
     <hr class="a-hr" />
     <div class="a-subnav-section entities <?php echo $classInfos[$class]['cssPlural'] ?>">
       <h4 data-toggles=".<?php echo $classInfos[$class]['cssPlural'] ?> .a-filter-options" class="a-slide-toggle filter-label <?php echo $sf_params->get('entity') ? 'open' : '' ?>"><?php echo a_($classInfos[$class]['plural']) ?></h4>
-      <div class="a-filter-options a-slide-toggled blog clearfix<?php echo ($sf_params->get('entity')) ? ' open' : '' ?>">
+      <?php $open = false ?>
+      <?php foreach ($items as $entity): ?>
+        <?php if ($entity['slug'] === $sf_params->get('entity')): ?>
+          <?php $open = true ?>
+        <?php endif ?>
+      <?php endforeach ?>
+      <div class="a-filter-options a-slide-toggled blog clearfix<?php echo $open ? ' open' : '' ?>">
         <?php foreach ($items as $entity): ?>
           <?php $selected_entity = ($entity['slug'] === $sf_params->get('entity')) ? $selected : array() ?>
           <div class="a-filter-option">
